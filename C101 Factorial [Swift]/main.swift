6a13f7e70722062b5f71a137810ad81cd87fdaaf
helen-giordano <helen.giordano@student.allenisd.org>
2022-11-15 09:05:54 -0600

    Recusion

---
+++ C101 Factorial [Swift]/main.swift
@@ -0,0 +1,47 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 7
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+
+var count = n
+
+if n > 1{
+    while count >= 1{
+        print("Calculating factorial(\(count))...")
+        count -= 1
+    }
+    print("Terminal case, returning 1.")
+}
+
+else{
+    print("Calculating factorial(\(n))...")
+    print("Terminal case, returning 1.")
+    print("The final result is: 1.")
+}
+
+
+
+count = n
+
+if n > 1{
+    var num1 = 1
+    var num2 = 2
+    var result = 0
+    while count > 1{
+        
+        result = num1*num2
+        print("Non-terminal case, returning \(result).")
+
+        count -= 1
+        num2 += 1
+        num1 = result
+    }
+    print("The final result is: \(result).")
+}
+
